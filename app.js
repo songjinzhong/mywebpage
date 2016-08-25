056830f9430b6df9740f83f47b889e0b05c3aec6
songjinzhong <648660165@qq.com>
2016-08-25 21:51:04 +0800

    add 404 page

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+/*if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
@@ -66,16 +66,13 @@ if (app.get('env') === 'development') {
       error: err
     });
   });
-}
+}*/
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.render('404', { title:'Page not found · 404' });
 });
 
 
