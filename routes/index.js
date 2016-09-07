411668db583c0dc90afa84405c4503190cdfbbee
songjinzhong <648660165@qq.com>
2016-09-07 22:16:23 +0800

    add /404 page

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,9 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: '关于我 | 渔人' });
 });
 
+/* GET 404 page. */
+router.get('/404', function(req, res, next) {
+  res.render('404', { title: '404 Page' });
+});
 
 module.exports = router;
